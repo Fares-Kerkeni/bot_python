28953933f7cf4e4d9ee4a19328a6736ddc86070c
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2022-05-25 12:05:00 +0200

    coucou sandie

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 client = discord.Client()
 
 from discord.ext import commands
@@ -8,16 +9,19 @@ default_intents.members= True
 client = discord.Client(intents=default_intents)
 client = commands.Bot(command_prefix="!")
 
-@client.command()
-async def test(ctx):
-    await ctx.send("test")
-
 @client.event
 async def on_member_join(member):
     welcome_channel = client.get_channel(978561467001487370)
     await welcome_channel.send('Bienvenue !' + member.display_name)
 
 # événement a chaque fois qu'un msg est envoyé
+
+joke = {
+    "Qu'est ce qui est jaune et qui attend" : "jonathan",
+    "Qu'est ce qui est transparent et qui cours dans un champ" : "un troupeau de vitre",
+    "qu'est ce qui clash tout le monde alors qu'elle est susceptible de baisée ?? " : "Fara"
+}
+
 #async -> en mm temp (plusieurs utilisateurs)
 @client.event
 async def on_message(message):
@@ -26,33 +30,28 @@ async def on_message(message):
 
     if message.author == client.user:
         return
-    # on vérifie que le msg ne vient pas de soi-même, cad le bot
 
-    Help_channel = client.get_channel(978559580592283669)
+    joke_channel = client.get_channel(978573528297250817)
 
-    # envoyer dans le channel "bonjour" si le msg !hello est envoyé dans le bon channel
-    if message.channel == Help_channel and message.content.startswith('!aide'):
-        await Help_channel.send('coucou')
+    if message.channel == joke_channel and message.content.startswith('!joke'):
+        await joke_channel.send('coucou')
 
     if message.content.startswith('!hello'):
-        await message.channel.send('coucou hibou ! :smiley:')
+        await message.channel.send('coucou')
 
     if message.content == "!del":
-        await message.channel.purge(limit=5)
+        await message.channel.purge(limit=10)
 
     await client.process_commands(message)  
     #avec cette ligne, si on écrit !test on recoit test du @client.command()
 
 # met le bot en ligne :
-client.run("OTc4NTU5Njg0MjQxOTQ0NTk2.GgybG2.lr-nnSvwkVttprTn7XcZnvtpR1Y5gwYW325K40")
 
 # -------------------------------- nolan --------------------------------
 
 
 
+
 # -------------------------------- nolan --------------------------------
 
-
-
-
-
+client.run("OTc4NTU5Njg0MjQxOTQ0NTk2.G8CrV2.sBKSZB__b9xOufWxN4f1VX96ZVKz-r9qlgwdTA")
